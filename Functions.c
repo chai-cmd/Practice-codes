14025354fd01a8fb6effab9f93e764367bd2abe5
Chaitanya Palve <chaitanyapalve1521@gmail.com>
2026-09-25 22:59:43 +0530

    Add files via upload

---
+++ Functions.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+    float average(int a, int b, int c){
+        return(a+b+c)/3.0;
+    }
+
+    int main(){
+        int a=3, b=4, c=5;
+       
+
+        printf("The average value is: %.2f\n", average(a,b,c));
+        return 0;  
+    }           
+
+
+
